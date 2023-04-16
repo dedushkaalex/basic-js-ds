05d49acd6680b6c2a4451e67e1f37df06d58880d
Aleksei Erchenko <proger@a1exxuss.ru>
2023-04-17 01:22:11 +0300

    bst functional

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,44 +6,111 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
-class BinarySearchTree {
-
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
   }
 }
+class BinarySearchTree {
+  constructor() {
+    this._root = null; // root bst
+  }
+  //возвращает корневой узел дерева
+  root() {
+    if (!this._root) return null;
+    else return this._root;
+  }
 
+  //добавить узел с data в дерево
+  add(data) {
+    let newNode = new Node(data);
+    if (this._root === null) {
+      this._root = newNode;
+    } else {
+      this.treeComparison(this._root, newNode)
+    }
+  }
+  treeComparison(node, newNode) {
+    if (newNode.data < node.data) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.treeComparison(node.left, newNode)
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.treeComparison(node.right, newNode)
+      }
+    }
+  }
+
+  //возвращает, true если узел с data существует в дереве и false в противном случае
+  has(data) {
+    let current = this._root;
+    let latestNode = null;
+    while (current != null) {
+      if (current.data > data) {
+        latestNode = current;
+        current = current.left;
+      } else if (current.data === data) {
+        return true;
+      }
+      else {
+        current = current.right
+
+      };
+    }
+    return false;
+  }
+
+  //возвращает узел с data, если узел с data существует в дереве и null в противном случае
+  find(data) {
+    let current = this._root;
+    while (current != null) {
+      if (current.data === data) {
+        return current;
+      } else if (current.data > data) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+    return null;
+  }
+
+  //удаляет узел с data из дерева, если узел с data существует
+  remove(data) {
+    
+  }
+
+
+  // возвращает минимальное значение, хранящееся в дереве (или null если в дереве нет узлов)
+  min() {
+    let minNode = this._root;
+    while (minNode.left !== null) {
+      minNode = minNode.left;
+    }
+    return minNode.data
+  }
+
+  //возвращает максимальное значение, хранящееся в дереве (или null если в дереве нет узлов)
+  max() {
+    let maxNode = this._root;
+    while (maxNode.right !== null) {
+      maxNode = maxNode.right;
+    }
+    return maxNode.data
+  }
+}
+const tree = new BinarySearchTree();
+tree.add(2);
+tree.add(3);
+tree.add(4);
+console.debug(tree.root().data)
 module.exports = {
   BinarySearchTree
 };
