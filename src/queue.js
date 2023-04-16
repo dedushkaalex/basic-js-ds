fd6838a3adc45067e9a184056262aa52059caec2
Aleksei Erchenko <proger@a1exxuss.ru>
2023-04-16 22:44:38 +0300

    queue

--- src/queue.js
+++ src/queue.js
@@ -13,21 +13,60 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
+
+
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = null;
+  }
+}
+class LinkedList extends Node {
+  constructor() {
+    super();
+    this.head = null;
+    this.tail = null;
+  }
+  append(value) {
+    const node = new Node(value)
+    if (!this.head || !this.tail) {
+      this.head = node;
+      this.tail = node;
+    }
+    this.tail.next = node;
+    this.tail = node;
+    return this;
+  }
+  getList() {
+    return this.head
+  }
+}
+
+class Queue extends LinkedList {
+  constructor() {
+    super();
+  }
 
   getUnderlyingList() {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    this.append(value);
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.head) {
+      return null;
+    }
+
+    const value = this.head.value;
+    this.head = this.head.next;
+    return value;
+  }
+  getUnderlyingList() {
+    return this.getList();
   }
 }
 
